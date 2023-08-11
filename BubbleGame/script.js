a1019e80856097b0b2fbbfb69ed6c942f077d376
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2023-08-11 22:51:04 +0500

    added timer

--- BubbleGame/script.js
+++ BubbleGame/script.js
@@ -1,12 +1,35 @@
+var panelContent = document.querySelector(".panel-content");
+var timerBox = document.querySelector(".timer-box");
 
+function bubbleMaker(panelContent) {
+  var query = "";
+  var panelWidth = panelContent.offsetWidth;
+  var panelHeight = panelContent.offsetHeight;
+  var bubbleSize = 40;
+  var bubblePerRow = panelWidth / bubbleSize;
+  var numRow = panelHeight / bubbleSize;
+  var totalBubbles = bubblePerRow * numRow;
 
-var query = "";
+  for (let i = 0; i <= totalBubbles; i++) {
+    var randomNum = Math.floor(Math.random() * 10);
+    query += ` <div class="bubble">${randomNum}</div> `;
+  }
 
-for (let i = 0; i <= 160; i++) {
-
-    var randomNum = Math.floor(Math.random()*10);
-    query +=` <div class="bubble">${randomNum}</div> `;
-    
+  document.querySelector(".panel-content").innerHTML = query;
 }
 
-document.querySelector(".panel-content").innerHTML = query;
+var time = 60;
+function timeFunction() {
+    var timerInterval = setInterval(() => {
+    if (time > 0) {
+      time--;
+      timerBox.innerHTML = time;
+    }
+    else{
+        clearInterval(timer);
+    }
+  }, 1000);
+}
+
+timeFunction();
+bubbleMaker(panelContent);

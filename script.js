a21ebdaacd19a2fe8e04316fe124401abd422923
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-07-09 16:05:12 +0500

    create 1 file and update 3 files : added new game + now games card will be shown by JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const mainContent = document.getElementsByClassName("main-content");
-const mainContentQS = document.querySelector(".main-content");
-const navBar = document.getElementsByClassName("nav-bar");
+const mainContent = document.querySelector(".main-content");
+const navBar = document.querySelector(".nav-bar");
 const gameText = document.getElementsByClassName("game-text");
 const navItem = document.getElementsByClassName("nav-item");
 const gameIcon = document.getElementsByClassName("game");
@@ -9,8 +8,8 @@ const darkModeIcon = document.getElementById("dark-mode-icon");
 const sidebarDarkModeIcon = document.getElementById("sidebar-dark-mode-icon");
 
 const onToggleDarkMode = () => {
-  if (darkModeIcon.classList.contains('fa-moon')) {
-    mainContent[0].style.backgroundColor = "#15202B";
+  if (darkModeIcon.classList.contains("fa-moon")) {
+    mainContent.style.backgroundColor = "#15202B";
     sideBar.style.backgroundColor = "rgb(1, 125, 63)";
     for (let i = 0; i < gameText.length; i++) {
       gameText[i].style.color = "#E4E6EB";
@@ -19,17 +18,16 @@ const onToggleDarkMode = () => {
       gameIcon[i].style.setProperty("--game-icon-hover-color", "#22303C");
     }
 
-    navBar[0].style.backgroundColor = "#03DAC5";
-    navBar[0].style.color = "#212628";
+    navBar.style.backgroundColor = "#03DAC5";
+    navBar.style.color = "#212628";
     for (let i = 0; i < navItem.length; i++) {
       navItem[i].style.color = "#212628";
     }
     darkModeIcon.className = "fa-solid fa-sun";
     sidebarDarkModeIcon.className = "fa-solid fa-sun";
-  }
-  else {
-    mainContent[0].style.backgroundColor = "white";
-    sideBar.style.backgroundColor = "rgb(0, 144, 72) ";
+  } else {
+    mainContent.style.backgroundColor = "white";
+    sideBar.style.backgroundColor = "rgb(0, 144, 72)";
     for (let i = 0; i < gameText.length; i++) {
       gameText[i].style.color = "#333333";
     }
@@ -37,30 +35,58 @@ const onToggleDarkMode = () => {
       gameIcon[i].style.setProperty("--game-icon-hover-color", "#dedede");
     }
 
-    navBar[0].style.backgroundColor = "#009090";
-    navBar[0].style.color = "white";
+    navBar.style.backgroundColor = "#009090";
+    navBar.style.color = "white";
     for (let i = 0; i < navItem.length; i++) {
       navItem[i].style.color = "white";
     }
     darkModeIcon.className = "fa-solid fa-moon";
     sidebarDarkModeIcon.className = "fa-solid fa-moon";
   }
-}
-
-mainContentQS.addEventListener("click", () => {
-  sideBar.style.width = 0;
-  mainContentQS.style.filter = "brightness(1)";
-});
+};
 
 const toggleSideBar = () => {
   const currentWidth = sideBar.style.width;
   if (currentWidth === "73vw") {
     sideBar.style.width = "0";
-    mainContentQS.style.filter = "brightness(1)";
-
+    mainContent.style.filter = "brightness(1)";
   } else {
     sideBar.style.width = "73vw";
-    mainContentQS.style.filter = "brightness(50%)";
-
+    mainContent.style.filter = "brightness(50%)";
   }
-}
+};
+
+// Rendering the main content dynamically
+const mainContentData = [
+  {
+    name: "Tic-Tac-Toe",
+    image: "assets/tic-tac-toe.png",
+    link: "tic-tac-toe/index.html",
+  },
+  {
+    name: "The Bubble Game",
+    image: "assets/the_bubble_game.png",
+    link: "BubbleGame/index.html",
+  },
+  {
+    name: "Dev Quiz",
+    image: "assets/dev_quiz.png",
+    link: "DevQuiz/index.html",
+  },
+  {
+    name: "HangMan",
+    image: "assets/hangman.png",
+    link: "https://hangmangame02.netlify.app/",
+  },
+];
+
+mainContentData.forEach(item => {
+  let newDiv = `
+     <a href=${item.link} class="game game1" target="_blank">
+        <img src=${item.image} alt="" />
+        <h3 class="game-text">${item.name}</h3>
+      </a>
+  `;
+
+  mainContent.innerHTML += newDiv;
+});
